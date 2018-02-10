8faf0c048ac7f9df05c3e883cd6e1687c7a6a02f
ghispi <ghispi@users.noreply.github.com>
2018-02-10 19:04:54 +0100

    deps fix
     - fixed composer deps
     - removed unnecesery prerequisites from readme

--- config/bundles.php
+++ config/bundles.php
@@ -11,4 +11,5 @@ return [
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
+    Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true, 'test' => true],
 ];
